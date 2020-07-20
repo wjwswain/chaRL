dd04dba18b7a1bc15ff244f9f6054238c6394a33
wjwswain <wswain40@gmail.com>
2020-07-20 11:11:23 -0500

    Transition to Gym-style environment

--- games/blackjack/Agent.py
+++ games/blackjack/Agent.py
@@ -1,5 +1,9 @@
-import rl as krl
+import rl.aents
 from tensorflow.keras import Input, Model
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.utils import plot_model
 import numpy as np
+
+
+
+class 
--- games/blackjack/GameEnv.py
+++ games/blackjack/GameEnv.py
@@ -1,11 +1,39 @@
 #https://dev.to/nexttech/build-a-blackjack-command-line-game-3o4b
+#https://stable-baselines.readthedocs.io/en/master/guide/custom_env.html
+
 import Deck
 import Hand
+import gym
+from gym import spaces
 
+class GameEnv(gym.Env):
+	metadata = {'render.modes': ['Human']}
 
-class Game:
     def __init__(self):
-        pass
+        super(Game, self).__init__()
+
+        self.bankroll = "unset"
+        while not self.bankroll.isnumeric():
+        	bankroll = input("Starting Bankroll:")
+        	try:
+            	bankroll = int(count)
+            	break
+            except ValueError:
+            	print('Not this time, Satan')
+            	continue
+
+        self.action_space = spaces.Dict({
+        	"bet":spaces.Discrete(100), #5(bet+1) = true bet (5-500)
+        	"hit_or_stay":spaces.Discrete(2) #stand, hit
+        	})
+
+        self.observation_space = spaces.Dict({
+        	"aces_left":spaces.Discrete(4),#0-3
+        	"cards_left":spaces.Discrete(49),#0-48
+        	"hand_value":spaces.Discrete(20),#2-21
+        	"dealer_value":spaces.Discrete(10),#2-11
+        	""
+        	})
 
     def play(self):
 
@@ -53,4 +81,4 @@ class Game:
                 b_checks = self.check_for_blackjack()
                 if True in b_checks:
                 	game_over = True
-                
+
