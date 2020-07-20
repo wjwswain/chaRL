ec572ed15e052ced278ecc9321ddda0823c246a3
wjwswain <wswain40@gmail.com>
2020-07-20 15:43:33 -0500

    Probably should have learned Python OOP, huh?

--- games/blackjack/GameEnv.py
+++ games/blackjack/GameEnv.py
@@ -7,33 +7,96 @@ import gym
 from gym import spaces
 
 class GameEnv(gym.Env):
-	metadata = {'render.modes': ['Human']}
+	metadata = {'render.modes': ['console']}
 
     def __init__(self):
         super(Game, self).__init__()
 
-        self.bankroll = "unset"
-        while not self.bankroll.isnumeric():
-        	bankroll = input("Starting Bankroll:")
-        	try:
-            	bankroll = int(count)
-            	break
-            except ValueError:
-            	print('Not this time, Satan')
-            	continue
+        self.bet = 0
+        self.rounds = 1
+        self.bankroll = 5000
+        self.name = "Example"
+        
+        self.deck = Deck()
+        self.hand = Hand(self.name)
+        self.dealer = Hand("Dealer")
 
-        self.action_space = spaces.Dict({
-        	"bet":spaces.Discrete(100), #5(bet+1) = true bet (5-500)
-        	"hit_or_stay":spaces.Discrete(2) #stand, hit
-        	})
+        self.observation = {}
+        self.reward = -1
+        self.done = False
+        self.info = {}
 
+        self.action_space = spaces.Dict({#... will ignore irrelevant moves
+        	"bet":spaces.Discrete(100), #5(bet+1) = true bet (5-500) 
+        	"hit_or_stand":spaces.Discrete(2) #hit=0, stand=1
+        })
         self.observation_space = spaces.Dict({
-        	"aces_left":spaces.Discrete(4),#0-3
-        	"cards_left":spaces.Discrete(49),#0-48
-        	"hand_value":spaces.Discrete(20),#2-21
-        	"dealer_value":spaces.Discrete(10),#2-11
-        	""
-        	})
+        	"stage":spaces.Discrete(2),#0=bet, 1=hit_or_stand
+        	"cards_left":spaces.MultiBinary(52),#deck binary array
+        	"hand_contents":spaces.MultiBinary(52),#hand binary array
+        	"hand_value":spaces.Discrete(21),#0, 2-21
+        	"dealer_contents":spaces.MultiBinary(52),#hand binary array
+        	"dealer_value":spaces.Discrete(11)#0, 2-11
+        })
+
+    def step(self, action):
+    	if self.observation["stage"] == 0:
+    		self.rounds += 1
+    		self.bet = max(5*(action["bet"]+1), self.bankroll)
+    		self.bankroll -= self.bet
+    		self.reward = -1
+
+    		
+    		self.hand = Hand(self.name)
+    		self.dealer = Hand("Dealer")
+
+    		first_card = self.deck.deal()
+    		second_card = self.deck.deal()
+    		dealer_card = self.deck.deal()
+
+
+
+    	elif action["hit_or_stand"] == 0:
+    		pass
+    	else:
+	    	if self.rounds == 500 or self.bankroll == 0:
+	    		self.done = True
+	    return self.observation, self.reward, self.done, self.info
+
+	def reset(self):
+		self.deck = Deck()
+		self.deck.shuffle()
+
+		self.observation = {
+		"stage":0,
+		"cards_left":np.ones(shape=52, dtype=np.int8),
+		"hand_contents":np.zeros(shape=52, dtype=np.int8),
+		"hand_value":0,
+		"dealer_contents":np.zeros(shape=52, dtype=np.int8),
+		"dealer_value":0
+		}
+
+	    return self.observation
+
+	def render(self, mode="console"):
+		if mode != "console":
+			raise NotImplementedError()
+		else:
+			if self.stage == 0:
+				if self.rounds != 0:
+					print(self.name, "bets", str(self.bet))
+				else:
+					print("Game Initializing...")
+					print()
+			else:
+				print(self.name + ': ' + str(self.bankroll))
+				print(self.hand.display())
+				print()
+				print("Dealer:", self.dealer.display())
+
+
+	def close (self):
+		pass
 
     def play(self):
 
@@ -45,26 +108,6 @@ class GameEnv(gym.Env):
 	        return b_checks
 
         while True:
-            self.deck = Deck()
-            self.deck.shuffle()
-
-            while True:
-            	bankroll = input("Starting Bankroll:")
-            	try:
-	            	bankroll = int(count)
-	            	break
-	            except ValueError:
-	            	print('Not this time, Satan')
-	            	continue
-
-            while True:
-	            count = input("Human Players:")
-	            try:
-	            	count = int(count)
-	            	break
-	            except ValueError:
-	            	print('Not this time either, Satan')
-	            	continue
 
 	        self.hands = []
 	        for pnum in range(count):
@@ -79,6 +122,4 @@ class GameEnv(gym.Env):
 	        game_over = False
             while not game_over:
                 b_checks = self.check_for_blackjack()
-                if True in b_checks:
-                	game_over = True
 
--- games/blackjack/Hand.py
+++ games/blackjack/Hand.py
@@ -1,7 +1,7 @@
 import Card
 
 class Hand:
-	def __init__(self, name, bankroll):
+	def __init__(self, name):
 		self.name = name
 		self.cards = []
 		self.value = 0
@@ -9,24 +9,11 @@ class Hand:
 	def hit(self, card):
 		self.cards.append(card)
 
-	def get_value(self):
-        self.value = 0
-        has_ace = False
-        for card in self.cards:
-            if card.value.isnumeric():
-                self.value += int(card.value)
-            elif card.value == 'A':
-                has_ace = True
-                self.value += 11
-            else:
-                self.value += 10
-        return self.value
-
     def display(self):
     	print(self.name)
     	card_list = self.cards
     	if self.name == "Dealer":
-    		card_list[0] == "??"
+    		print("??")
     	for card in card_list:
     		print(card)
     	print("Value:", self.value)
